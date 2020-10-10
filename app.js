be2aee1364fa8f82036bb037c63101eaeae0fdd6
tetra-shayeb <mohammednm40@gmail.com>
2020-10-10 17:12:45 +0300

    finish add new to do

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const express = require("express");
 const app = express();
 const wetherRoute = require('./routes/weather');
+// Middleware
+app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }))
 
-app.use('/', wetherRoute);
-app.set('view engine', 'ejs')
+// Import Route
+const weatherRoute = require('./routes/weather');
 
+// Use View Engine
+app.set('view engine', 'ejs');
+
+// Middleware route
+app.use('/', weatherRoute);
 
 const port = 3000;
 app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
+    console.log(`Example app listening at http://localhost:${port}`);
 });
--- routes/weather.js
+++ routes/weather.js
@@ -1,7 +1,14 @@
 const router = require('express').Router();
+const fetch = require ('node-fetch')
 
-router.get('/', (req, res) => {
-    res.send('Hello World')
-});
+router.post('/', (req, res) => {
+    const city = req.body.city;
+    const url_api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`
+
+    fetch(url_api)
+    .then(res => res.json())
+    .then(data => console.log(data));
+})
+
 
 module.exports = router;
