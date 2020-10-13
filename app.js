3ceee8f95b5c801e622850fe1f212ab6d8d06b82
tetra-shayeb <mohammednm40@gmail.com>
2020-10-13 12:45:54 +0300

    finish add new todo

--- app.js
+++ app.js
@@ -27,16 +27,30 @@ app.get('/users', (req, res) => {
 
 app.post('/users', async (req, res) => {
     try {
-      const hashedPassword = await bcrypt.hash(req.body.password, 10)
-      const user = { name: req.body.name, password: hashedPassword }
-      users.push(user)
-      res.status(201).send()
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+        const user = { name: req.body.name, password: hashedPassword };
+        users.push(user);
+        res.status(201).send();
     } catch {
-      res.status(500).send()
+        res.status(500).send();
     }
-  })
+});
 
-
+app.post('/users/login', async (req, res) => {
+    const user = users.find(user.name === req.body.name);
+    if (user == null) {
+        return res.status(400).send('Cannot find user');
+    };
+    try {
+        if (await bcrypt.compare(req.body.password, user.password)) {
+            res.send('Succes')
+        } else {
+            res.send('not Allowed')
+        }
+    } catch {
+        res.status(500).send()
+    }
+})
 
 const port = 3000;
 app.listen(port, () => {
